511528f92212a2462b6cb2880317ed82da1d5f72
Charanyv1 <charanyv31@gmail.com>
2026-04-18 23:56:54 +0530

    Update Cal.java

--- Cal.java
+++ Cal.java
@@ -1,41 +1,16 @@
-import java.util.Scanner;
-
 public class Cal {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        double a = sc.nextDouble();
-
-        System.out.print("Enter second number: ");
-        double b = sc.nextDouble();
-
-        System.out.print("Enter operator (+, -, *, /): ");
-        char op = sc.next().charAt(0);
+        double a = 10;
+        double b = 5;
+        char op = '+';
 
         double result = 0;
 
         switch(op) {
-            case '+':
-                result = a + b;
-                break;
-            case '-':
-                result = a - b;
-                break;
-            case '*':
-                result = a * b;
-                break;
-            case '/':
-                if(b != 0)
-                    result = a / b;
-                else {
-                    System.out.println("Cannot divide by zero");
-                    return;
-                }
-                break;
-            default:
-                System.out.println("Invalid operator");
-                return;
+            case '+': result = a + b; break;
+            case '-': result = a - b; break;
+            case '*': result = a * b; break;
+            case '/': result = a / b; break;
         }
 
         System.out.println("Result = " + result);
